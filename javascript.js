8131fe07ab5a4b3eb5f4bf16bced017c53084fff
migena <migena.allushi1@gmail.com>
2023-11-29 17:28:25 +0100

    created operations functions

---
+++ javascript.js
@@ -0,0 +1,19 @@
+//create addition function
+function add(a, b){
+    return a + b;
+};
+//create subtraction function
+function subtract(a, b){
+    return a -b;
+};
+//create multiplication function
+function multiply(a, b){
+    return a * b;
+};
+//create division function
+function divide(a, b){
+    return a / b;
+};
+//create 3 variables for the 3 parts of calculation operation
+//create a function, operate, that takes 2nb and then calls one fx
+
